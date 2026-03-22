a723849b4a1a9418aa5ed111dfe66f752bc42f3b
phamhoan-caee <phamhoan.caee@gmail.com>
2026-03-22 17:19:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -196,7 +196,7 @@ async function submitQuiz() {
     alert(`Chúc mừng! Kết quả của bạn: ${score}/30 câu - Trạng thái: ${status}`);
 
    // --- GỬI DỮ LIỆU VỀ GOOGLE SHEETS ---
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbzFDyujjPPP5EezT_-4F4vs9-9B77c_dNmwdWP5TyolKzj39Im9aT0ybRauIt_R6Bev/exec';
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbwNkK9MVz7IGECvsXJvqoN7gQXfHI189DLuMgy6m4_tc3W2W5lGD35hy5TmdbkNYAGJGA/exec';
     
     const payload = {
         name: document.getElementById('studentName').value, // Gửi cho cột HOTEN
